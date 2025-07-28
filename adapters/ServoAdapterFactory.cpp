6c1dc96a56ed79151dd231feade9643b7457861c
rxchen <rxchen1999@163.com>
2025-07-28 21:26:24 +0800

    加入工厂模式

---
+++ adapters/ServoAdapterFactory.cpp
@@ -0,0 +1,3 @@
+#include "ServoAdapterFactory.h"
+
+ServoAdapterFactory::ServoAdapterFactory() {}
---
+++ adapters/ServoAdapterFactory.h
@@ -0,0 +1,10 @@
+#ifndef SERVOADAPTERFACTORY_H
+#define SERVOADAPTERFACTORY_H
+
+class ServoAdapterFactory
+{
+public:
+    ServoAdapterFactory();
+};
+
+#endif // SERVOADAPTERFACTORY_H
